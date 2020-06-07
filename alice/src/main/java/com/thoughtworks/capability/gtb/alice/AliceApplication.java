f6e9084dc9cdd45a353cbb004b634d523f92d435
he.zhu <he.zhu@thgoughtworks.com>
2020-06-07 23:41:50 +0800

    docker compose homework

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceApplication.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceApplication.java
@@ -3,9 +3,11 @@ package com.thoughtworks.capability.gtb.alice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class AliceApplication {
 
   public static void main(String[] args) {
